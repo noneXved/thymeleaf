a11d2c240bc9f1eb475a094964fad111de3a86eb
still_worth <bajno.damian96@gmail.com>
2020-10-28 19:51:50 +0100

    Adding new objects to lists and print

--- src/main/java/com/example/damian/bajno/thymeleaf/CarController.java
+++ src/main/java/com/example/damian/bajno/thymeleaf/CarController.java
@@ -3,25 +3,37 @@ package com.example.damian.bajno.thymeleaf;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class CarController {
 
-    @GetMapping("/car")
-    public String getCar(Model model) {
+    private List<Car> cars;
+
+    public CarController() {
         Car car = new Car("Volkswagen", "Golf");
         Car car1 = new Car("BMW", "e36");
         Car car2 = new Car("Ferrari", "Pierro");
         Car car3 = new Car("Porshe", "Cayman");
         Car car5 = new Car("Chevrolet", "Roadster");
+         cars = new ArrayList<>(List.of(car, car1, car2, car3, car5));
+    }
 
-        List<Car> cars = new ArrayList<>(List.of(car, car1, car2, car3, car5));
-
+    @GetMapping("/car")
+    public String getCar(Model model) {
         model.addAttribute("cars", cars);
-
+        model.addAttribute("newCar", new Car());
         return "car";
+    }
+
+    @PostMapping("/add-car")
+    public String addCar(@ModelAttribute Car car) {
+        cars.add(car);
+        return "redirect:/car";
     }
 }
